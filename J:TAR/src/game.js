c49cce97d2962f9ebc2194d40a9753482843aeb3
Jabiveir <107405764+Jabiveir@users.noreply.github.com>
2024-01-12 22:33:51 -0500

    Update game.js

--- J:TAR/src/game.js
+++ J:TAR/src/game.js
@@ -1,12 +1,25 @@
 export default class Game {
   constructor(Ctx, Player) {
+    let Error = false;
+    window.onerror = function () {
+      Error = true;
+      Ctx.fillStyle = "#f00";
+      Ctx.fillRect(140, 180, 440, 220);
+      Ctx.fillStyle = "#000";
+      Ctx.font = "120px sans-serif";
+      Ctx.fillText("ERROR", 360, 300);
+      Ctx.font = "40px sans-serif";
+      Ctx.fillText("Game frozen to prevent", 360, 345);
+      Ctx.fillText("browser crash.", 360, 380);
+    };
     var limitLoop = function (fn) {
       var then = new Date().getTime();
       var interval = 1000 / 60;
       return (function loop(time) {
         requestAnimationFrame(loop);
         var now = new Date().getTime();
-        if (now - then > interval) {
+        let delta = now - then;
+        if (delta > interval && !Error) {
           then = now - (delta % interval);
           fn();
         }
