1fd6dcd0146977e8224402ffa1b4acd95ea17f47
Jabiveir <107405764+Jabiveir@users.noreply.github.com>
2024-01-12 22:44:16 -0500

    Update levels.js

--- J:TAR/src/levels.js
+++ J:TAR/src/levels.js
@@ -59,6 +59,7 @@ export default class levels {
   }
 
   DrawLevel(Ctx, Level, Player) {
+    throw "This is an error";
     for (let i = 0; i < Levels[Level].length; i++)
       Levels[Level][i].DrawBlock(Ctx, Player);
   }
