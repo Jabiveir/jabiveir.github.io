c4fa5fb23ec4cd36922c944404c4c416c94444ed
Jabiveir <107405764+Jabiveir@users.noreply.github.com>
2024-08-30 21:40:47 -0400

    Update index.js

--- J:TA/src/index.js
+++ J:TA/src/index.js
@@ -21,7 +21,7 @@ let Jabiveir = new Player();
 new InputHandler(Jabiveir, Filler, Ctx);
 
 let Level = 0;
-let name = "Level=";
+let name = "JTALevel=";
 let decodedCookie = decodeURIComponent(document.cookie);
 let ca = decodedCookie.split(";");
 for (let i = 0; i < ca.length; i++) {
