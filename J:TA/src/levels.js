11bcc4ee9c03a0aa97fb3c5eb2cf73a58d096008
Jabiveir <107405764+Jabiveir@users.noreply.github.com>
2024-08-30 21:41:01 -0400

    Update levels.js

--- J:TA/src/levels.js
+++ J:TA/src/levels.js
@@ -55,7 +55,7 @@ export default class levels {
 		let d = new Date();
 		d.setTime(d.getTime() + 365 * 24 * 60 * 60 * 1000);
 		document.cookie =
-			"Level=" + Player.PData.Level + ";expires=" + d.toUTCString() + ";path=/";
+			"JTALevel=" + Player.PData.Level + ";expires=" + d.toUTCString() + ";path=/";
 	}
 
 	ResetLevel(Ctx, Player) {
