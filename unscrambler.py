bf91a32c90c85d00c2ad4b682f32dceaef5c388a
Jabiveir <107405764+Jabiveir@users.noreply.github.com>
2025-03-12 16:48:49 -0400

    Update unscrambler.py

--- unscrambler.py
+++ unscrambler.py
@@ -11,6 +11,7 @@ def unscramble(L):
     Res.append(b[len(b)-1])
   if len(A) % 2 == 1:
     Res.pop(len(Res)-1)
+    Res.pop(len(Res)-2)
   else:
     Res.pop(len(Res)-3)
     Res.pop(len(Res)-2)
@@ -18,16 +19,13 @@ def unscramble(L):
   return Res
 
 def get_flag():
-  flag = open('flag.txt', 'r').read()
-  flag = flag.strip()
-  hex_flag = []
-  for c in flag:
-    hex_flag.append([str(hex(ord(c)))])
+  flag = open('test.txt', 'r').read()
+  flag = eval(flag.strip())
 
-  return hex_flag
+  return flag
 
 def main():
-  flag = eval(input("Input data: "))
+  flag = get_flag()
   cypher = unscramble(flag)
   for i in cypher:
     print(i, end=" ")
