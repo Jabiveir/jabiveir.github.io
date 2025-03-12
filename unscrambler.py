96b969fdebfe0d9ad0e4aa5e8fe2435e631fb628
Jabiveir <107405764+Jabiveir@users.noreply.github.com>
2025-03-12 16:28:22 -0400

    Create unscrambler.py

---
+++ unscrambler.py
@@ -0,0 +1,35 @@
+import sys
+
+def exit():
+  sys.exit(0)
+
+def unscramble(L):
+  A = L
+  Res = []
+  for b in A:
+    Res.append(b[0])
+    Res.append(b[len(b)-1])
+  if len(A) % 2 == 1:
+    Res.pop(len(Res)-1)
+  else:
+    Res.pop(len(Res)-3)
+    Res.pop(len(Res)-2)
+    
+  return Res
+
+def get_flag():
+  flag = open('flag.txt', 'r').read()
+  flag = flag.strip()
+  hex_flag = []
+  for c in flag:
+    hex_flag.append([str(hex(ord(c)))])
+
+  return hex_flag
+
+def main():
+  flag = eval(input("Input data: "))
+  cypher = unscramble(flag)
+  print(cypher)
+
+if __name__ == '__main__':
+  main()
